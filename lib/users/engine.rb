003082f1d1610c4ba3c7f6103224357f220f99d5
develumpencom <develumpen@breakabletoys.com>
2025-02-25 19:03:32 -0600

    current attributes are now included in the Current model of the main app (#13)

--- lib/users/engine.rb
+++ lib/users/engine.rb
@@ -1,5 +1,11 @@
 module Users
   class Engine < ::Rails::Engine
     isolate_namespace Users
+
+    config.to_prepare do
+      if defined?(::Current) && ::Current < ActiveSupport::CurrentAttributes
+        ::Current.include CurrentAttributes
+      end
+    end
   end
 end
